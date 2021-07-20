52026bcc48aa048a1d647f6e9d9e7e888e385479
xiaopengli89 <x.friday@outlook.com>
2021-07-20 12:48:40 +0800

    Release v0.1.4

--- src/main.rs
+++ src/main.rs
@@ -306,7 +306,7 @@ impl Powershell {
 }
 
 #[derive(Clap, Debug, Clone)]
-#[clap(version = "0.1.3", author = "Xiaopeng Li <x.friday@outlook.com>")]
+#[clap(version = "0.1.4", author = "Xiaopeng Li <x.friday@outlook.com>")]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
     #[clap(short, long)]
