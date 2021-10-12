fd9b63cfc19454bc9e0055ae4a8e1e0c08127a0b
xiaopengli89 <x.friday@outlook.com>
2021-10-12 13:39:43 +0800

    Release v0.2.0

--- src/main.rs
+++ src/main.rs
@@ -376,7 +376,7 @@ impl CpuInfo {
 }
 
 #[derive(Clap, Debug, Clone)]
-#[clap(version = "0.1.10", author = "Xiaopeng Li <x.friday@outlook.com>")]
+#[clap(version = "0.2.0", author = "Xiaopeng Li <x.friday@outlook.com>")]
 #[clap(setting = AppSettings::ColoredHelp)]
 struct Opts {
     #[clap(short, long)]
