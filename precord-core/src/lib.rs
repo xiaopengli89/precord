c2a81424eded6bae614b9e447a6fd4306ea0cbff
xiaopengli89 <x.friday@outlook.com>
2021-11-03 19:06:49 +0800

    Fix definition of Pid

--- precord-core/src/lib.rs
+++ precord-core/src/lib.rs
@@ -3,4 +3,8 @@ pub use system::{System, Features};
 mod platform;
 mod system;
 
+#[cfg(unix)]
 pub type Pid = libc::pid_t;
+
+#[cfg(target_os = "windows")]
+pub type Pid = winapi::shared::minwindef::DWORD;
