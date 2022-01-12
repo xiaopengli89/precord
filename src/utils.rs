27b93334a3a70b1957922f1851277fc10f8d25be
xiaopengli89 <x.friday@outlook.com>
2022-01-12 18:19:09 +0800

    Adjust SeDebugPrivilege

--- src/utils.rs
+++ src/utils.rs
@@ -152,3 +152,82 @@ impl From<&str> for Command {
         }
     }
 }
+
+pub fn adjust_privileges() {
+    #[cfg(target_os = "windows")]
+    platform_windows::adjust_privileges();
+}
+
+#[cfg(target_os = "windows")]
+mod platform_windows {
+    use std::ffi::OsStr;
+    use std::mem::MaybeUninit;
+    use std::os::windows::ffi::OsStrExt;
+    use std::{mem, ptr};
+    use winapi::shared::minwindef::{FALSE, TRUE};
+    use winapi::um::handleapi::CloseHandle;
+    use winapi::um::processthreadsapi::{GetCurrentProcess, OpenProcessToken};
+    use winapi::um::securitybaseapi::AdjustTokenPrivileges;
+    use winapi::um::winbase::LookupPrivilegeValueW;
+    use winapi::um::winnt::{
+        HANDLE, LUID, SE_DEBUG_NAME, SE_PRIVILEGE_ENABLED, TOKEN_ADJUST_PRIVILEGES,
+        TOKEN_PRIVILEGES,
+    };
+    use windows::Win32::Foundation::{GetLastError, ERROR_NOT_ALL_ASSIGNED};
+
+    struct HandleWrapper(HANDLE);
+
+    impl Drop for HandleWrapper {
+        fn drop(&mut self) {
+            unsafe {
+                let _ = CloseHandle(self.0);
+            }
+        }
+    }
+
+    pub fn adjust_privileges() {
+        unsafe {
+            let current_handle = HandleWrapper(GetCurrentProcess());
+            let mut token_handle: HANDLE = MaybeUninit::uninit().assume_init();
+            let r = OpenProcessToken(current_handle.0, TOKEN_ADJUST_PRIVILEGES, &mut token_handle);
+            if r != TRUE {
+                eprintln!("OpenProcessToken failed");
+                return;
+            }
+            let token_handle = HandleWrapper(token_handle);
+
+            let mut luid: LUID = MaybeUninit::uninit().assume_init();
+            if LookupPrivilegeValueW(ptr::null(), to_wchar(SE_DEBUG_NAME).as_ptr(), &mut luid)
+                != TRUE
+            {
+                eprintln!("LookupPrivilegeValueW failed");
+                return;
+            }
+
+            let mut new_state: TOKEN_PRIVILEGES = MaybeUninit::uninit().assume_init();
+            new_state.PrivilegeCount = 1;
+            new_state.Privileges[0].Luid = luid;
+            new_state.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+            if AdjustTokenPrivileges(
+                token_handle.0,
+                FALSE,
+                &mut new_state,
+                mem::size_of::<TOKEN_PRIVILEGES>() as _,
+                ptr::null_mut(),
+                ptr::null_mut(),
+            ) != TRUE
+                || GetLastError() == ERROR_NOT_ALL_ASSIGNED
+            {
+                eprintln!("AdjustTokenPrivileges failed");
+            }
+        }
+    }
+
+    fn to_wchar(str: &str) -> Vec<u16> {
+        OsStr::new(str)
+            .encode_wide()
+            .chain(Some(0).into_iter())
+            .collect()
+    }
+}
