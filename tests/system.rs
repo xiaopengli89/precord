5fc9f9b524150f2f51d814450827abc036ac5ccd
XiaoPeng <lixiaopeng.jetspark@bytedance.com>
2024-07-09 19:48:35 +0800

    fix tests

--- tests/system.rs
+++ tests/system.rs
@@ -1,5 +1,5 @@
 #[test]
 fn two_smc() {
-    let _ = precord_core::System::new(precord_core::Features::SMC, []).unwrap();
-    let _ = precord_core::System::new(precord_core::Features::SMC, []).unwrap();
+    let _ = precord_core::System::new(precord_core::Features::SMC, []);
+    let _ = precord_core::System::new(precord_core::Features::SMC, []);
 }
